cb3aa9ef767127b4f71a3016610313c7a4eb7a4e
john <bblodfon@gmail.com>
2021-11-23 21:30:27 +0100

    add tests for pearson coefficient calculation

--- test/task_test.py
+++ test/task_test.py
@@ -1,4 +1,6 @@
+import math
 import unittest
+import warnings
 
 from src.task import ElixirTask
 
@@ -51,5 +53,28 @@ class TaskTest(unittest.TestCase):
         list2 = [[1, 7], [8, 10], [14, 15], [15, 20]]
         self.assertEqual(task.get_overlap(list1, list2), 5)
 
+    def test_pearson_cor(self):
+        task = ElixirTask()
+
+        x = list(range(0,10))
+        y = list(range(1,11))
+        self.assertEqual(task.pearson_cor(x, y), 1.0)
+
+        z = list(reversed(range(0,10)))
+        self.assertEqual(task.pearson_cor(x, z), -1.0)
+
+        # the example in the task description
+        X = [10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0]
+        Y = [10.5, 11.5, 12.0, 13.0, 13.5, 15.0, 14.0]
+        self.assertAlmostEqual(task.pearson_cor(X, Y), 0.9452853, places=7)
+
+        # constant list example
+        w = 10 * [1]
+        with self.assertWarns(Warning):
+            self.assertTrue(math.isnan(task.pearson_cor(w, z)))
+
+
 if __name__ == '__main__':
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', category=Warning)
     unittest.main()
